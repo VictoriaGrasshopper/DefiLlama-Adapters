6be7310f12b31b1be6bdf049706eedf0bfd672db
Victoria Horbunova <vimirabo@gmail.com>
2026-09-23 09:19:30 +0200

    feat: update OGY supply endpoint to gateway.origyn.com

--- projects/origyn/index.js
+++ projects/origyn/index.js
@@ -1,22 +1,25 @@
 const { toUSDTBalances } = require("../helper/balances");
 const { get } = require('../helper/http')
 
-async function staking(api) {
-  // Get the total supply in OGY
-  // Ref: https://github.com/ORIGYN-SA/origyn-sns/blob/master/backend/canisters/token_metrics/impl/src/queries/http_request.rs#L40
-  const totalSupplyUrl = 'https://juolv-3yaaa-aaaal-ajc6a-cai.raw.icp0.io/total-supply';
-  const totalSupply = await get(totalSupplyUrl);
+async function staking() {
+  // Get the OGY supply metrics
+  // Ref: https://gateway.origyn.com/docs/#/Tokens/get_origyn_supply_summary
+  const supplyUrl = 'https://gateway.origyn.com/v1/tokens/OGY/supply/summary';
+  const supplyData = await get(supplyUrl);
+  const totalSupply = Number(BigInt(supplyData.total_supply)) / 1e8;
+  const circulatingSupply = Number(BigInt(supplyData.circulating_supply)) / 1e8;
   
-  // Get the circulating supply in OGY
-  // Ref: https://github.com/ORIGYN-SA/origyn-sns/blob/master/backend/canisters/token_metrics/impl/src/queries/http_request.rs#L43
-  const circulatingSupplyUrl = 'https://juolv-3yaaa-aaaal-ajc6a-cai.raw.icp0.io/circulating-supply';
-  const circulatingSupply = await get(circulatingSupplyUrl);
+  
+  // Get the OGY/USDT market price
+  const ogyPriceUrl = 'https://api.origyn.com/ogy/price';
+  const ogyPriceData = await get(ogyPriceUrl);
+  const ogyUsdt = ogyPriceData.ogyPrice;
 
   // The token value locked is the locked supply
   // total supply - circulating supply
-  const tokenTvl = (totalSupply - circulatingSupply);
+  const tokenTvl = (totalSupply - circulatingSupply) * ogyUsdt;
 
-  api.addCGToken('origyn-foundation', tokenTvl)
+  return toUSDTBalances(tokenTvl);
 }
 
 async function tvl() {
@@ -31,7 +34,9 @@ async function tvl() {
 
 module.exports = {
   timetravel: false,
-  misrepresentedTokens: true,
   methodology: "TVL the total locked value of staked tokens and the total asset value of ORIGYN certificates.",
-  icp: { tvl, staking },
+  icp: {
+    tvl,
+    staking,
+  },
 }
